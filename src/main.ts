bfad492e75d331447e462cceceaa2b22cfdac18e
Ryuta Sakai <info@sakairyuta.com>
2022-08-01 22:40:59 +0900

    feat: blur on click totop

--- src/main.ts
+++ src/main.ts
@@ -11,10 +11,17 @@ anchorLinks.forEach((anchorLink) => {
       e.preventDefault();
       // NOTE: location.hash = '' は動作しないのでhistory apiを使用する
       history.pushState('', document.title, location.pathname + location.search);
+      /**
+       * NOTE: クリックしたアンカーのフォーカスを外す。（ preventDefault()しない場合のブラウザの挙動に合わせる ）
+       * 標準ではtargetがfocusable areaの場合はtargetにフォーカスを、その他の場合はviewport(body)をフォールバックにする。
+       * @see https://html.spec.whatwg.org/multipage/browsing-the-web.html#scroll-to-the-fragment-identifier
+       */
+      anchorLink.blur();
       window.scrollTo({
         top: 0,
         behavior: 'smooth',
       });
+      return;
     }
     document.documentElement.style.scrollBehavior = 'smooth';
     // 一定時間後にscroll-behavior: smooth;を削除する。
